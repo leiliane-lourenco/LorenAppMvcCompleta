b5a30a3f12ca1892d31cc4a24ae9adf82501da8b
Leiliane Lourenco Santos Quirino <Leilianelourenzo@gamail.com>
2023-01-12 19:40:48 -0300

    **Configurado tratamento de erros

--- src/Loren.Api/Startup.cs
+++ src/Loren.Api/Startup.cs
@@ -44,7 +44,8 @@ namespace Loren.Api
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/erro/500");
+                app.UseStatusCodePagesWithRedirects("/erro/{0}");
                 app.UseHsts();
             }
 
